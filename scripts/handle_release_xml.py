183d07512c3ecfa76e800f80c146f9c3fa330fc0
Steven BOEHM <steven.boehm.dev@gmail.com>
2022-08-11 00:46:20 +0200

    still WIP cleanup

--- scripts/handle_release_xml.py
+++ scripts/handle_release_xml.py
@@ -4,20 +4,20 @@ import sys
 import xml.etree.ElementTree as etree
 import urllib
 import time
-from typing import List
+from typing import List, Tuple
 
 
 class DiscogsReleasesXMLParser:
-    """TODO."""
+    """Parser for Discogs release dump files."""
 
     def __init__(self, discogs_client, input_file, images_dir):
-        """TODO."""
+        """Init DiscogsReleasesXMLParser with."""
         self.discogs_client = discogs_client
         self.input_file = input_file
         self.images_dir = images_dir
 
     def list_saved_release_id(self) -> List[int]:
-        """TODO."""
+        """Return a list of all saved releases ID."""
         saved_releases = []
         for path, subdirs, files in os.walk(self.images_dir):
             for name in [f for f in files if "primary" in f]:
@@ -28,7 +28,7 @@ class DiscogsReleasesXMLParser:
         return saved_releases
 
     def get_latest_saved_release_id(self) -> int:
-        """TODO."""
+        """Return the latest saved release ID."""
         saved_release_ids = self.list_saved_release_id()
         return saved_release_ids[-1]
 
@@ -36,46 +36,37 @@ class DiscogsReleasesXMLParser:
         """TODO."""
         start_time = time.time()
         latest_saved_release_id = self.get_latest_saved_release_id()
+
         for event, elem in etree.iterparse(self.input_file, events=("start",)):
             if elem.tag == "release":
-                formats = elem.find('formats')
-                is_vinyl_release = formats is not None and any(f.get('name') == 'Vinyl' for f in formats)
-                master = elem.find('master_id')
+                try:
+                    master_id, release_id = validate_xml_release(
+                        elem,
+                        latest_saved_release_id
+                    )
+                    release = self.discogs_client.release(release_id)
+                    front_cover_url = get_front_cover_url(release)
+                except Exception as err:
+                    print(err, file=sys.stderr)
+                    elem.clear()
+                    continue
 
-                if is_vinyl_release and master is not None:
-                    release_id = elem.get('id')
-                    if int(release_id) <= latest_saved_release_id:
-                        print("Already parsed release {}".format(release_id))
-                        elem.clear()
-                        continue
-                    master_id = master.text
-
-                    try:
-                        front_cover_url = get_front_cover_url(self.discogs_client, elem)
-                    except AlbumCoverURLNotFound as err:
-                        print(err, file=sys.stderr)
-                        elem.clear()
-                        continue
-
-                    filename, file_extension = os.path.splitext(front_cover_url)
-                    filename = "{}_{}_primary{}".format(release_id, master_id, file_extension)
-                    filepath = os.path.join(self.images_dir, filename)
-                    if os.path.exists(filepath):
-                        elem.clear()
-                        continue
-                    end_time = time.time()
-                    ellapsed_time = end_time - start_time
-                    sleep_time = max((1.02 - ellapsed_time), 0)
-                    time.sleep(sleep_time)
-                    try:
-                        urllib.request.urlretrieve(front_cover_url, filepath)
-                    except:
-                        print(
-                            "Downloading cover failed for release {} ({})"
-                            .format(release_id, front_cover_url)
-                        )
-                    start_time = time.time()
-                    print(filepath)
+                filename, ext = os.path.splitext(front_cover_url)
+                filename = "{}_{}_primary{}".format(release_id, master_id, ext)
+                filepath = os.path.join(self.images_dir, filename)
+                end_time = time.time()
+                ellapsed_time = end_time - start_time
+                sleep_time = max((1.02 - ellapsed_time), 0)
+                time.sleep(sleep_time)
+                try:
+                    urllib.request.urlretrieve(front_cover_url, filepath)
+                except:
+                    print(
+                        "Downloading cover failed for release {} ({})"
+                        .format(release_id, front_cover_url)
+                    )
+                start_time = time.time()
+                print(filepath)
 
             elem.clear()
 
@@ -86,12 +77,69 @@ class AlbumCoverURLNotFound(Exception):
     pass
 
 
-def get_front_cover_url(discogs_client, xml_release) -> str:
+class UnprocessableRelease(Exception):
+    """Raise when a release is considered unprocessable.
+
+    For example: an album without vinyl release,
+    or without valid master_id and release_id
+    """
+
+    pass
+
+
+def validate_xml_release(xml_release, last_release_id=None) -> Tuple[int, int]:
+    """Return the given release's master_id and release_id.
+
+    Args:
+        xml_release (:obj): a <release> ElementTree from a discogs release dump
+        last_release_id (int, optional): the latest saved release_id. Defaults
+            to None. If given, all inferior releases will be considered treated
+            and thus "Unprocessable".
+
+    Returns:
+        int: The master_id of the release
+        int: The release_id.
+
+    Raises:
+        UnprocessableRelease: The release cannot be processed.
+    """
+    release_id = xml_release.get('id')
+    if release_id is None:
+        raise UnprocessableRelease("Could not determine release_id ")
+    release_id = int(release_id)
+
+    if (last_release_id is not None and release_id <= last_release_id):
+        raise UnprocessableRelease(
+            "Already parsed release {}".format(release_id)
+        )
+
+    formats = xml_release.find('formats')
+    if formats is None:
+        raise UnprocessableRelease(
+            "No format associated with release {}".format(release_id)
+        )
+
+    is_vinyl_release = any(f.get('name') == 'Vinyl' for f in formats)
+    if not is_vinyl_release:
+        raise UnprocessableRelease(
+            "No vinyl associated with release {}".format(release_id)
+        )
+
+    master = xml_release.find('master_id')
+    if master is None or master.text is None:
+        raise UnprocessableRelease(
+            "Could not find master_id for release {}".format(release_id)
+        )
+    master_id = int(master.text)
+
+    return master_id, release_id
+
+
+def get_front_cover_url(release) -> str:
     """Return the given release's front cover URL.
 
     Args:
-        discogs_client: an authenticated instance of discogs_client
-        xml_release: a <release> ElementTree from a discogs release dump
+        release (:obj): a "Release" from the discogs_client
 
     Returns:
         str: The URL of the album relase front cover.
@@ -99,26 +147,9 @@ def get_front_cover_url(discogs_client, xml_release) -> str:
     Raises:
         AlbumCoverURLNotFound: The front cover URL could not been found.
     """
-    release_id = xml_release.get('id')
-    xml_master = xml_release.find('master_id')
-    if xml_master is None or xml_master.text is None:
-        raise AlbumCoverURLNotFound(
-            "Could not find master_id associated with release {}"
-            .format(release_id)
-        )
-    try:
-        release = discogs_client.release(release_id)
-    except json.decoder.JSONDecodeError:
-        raise AlbumCoverURLNotFound(
-            "Decoding release {} failed".format(release_id)
-        )
-    except:
-        raise AlbumCoverURLNotFound(
-            "Discogs client failed fetching release {}".format(release_id)
-        )
     if release.images is None:
         raise AlbumCoverURLNotFound(
-            "No images for release {}".format(release_id)
+            "No images for release {}".format(release.id)
         )
     front_cover = next(
         filter(lambda x: x.get('type') == 'primary', release.images),
@@ -126,7 +157,7 @@ def get_front_cover_url(discogs_client, xml_release) -> str:
     )
     if front_cover is None:
         raise AlbumCoverURLNotFound(
-            "No front cover for release {}".format(release_id)
+            "No front cover for release {}".format(release.id)
         )
 
     return front_cover.get('uri')
