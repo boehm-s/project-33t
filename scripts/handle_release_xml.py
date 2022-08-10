6f8bf531b12a2dfbabefff98fb689b59b080f810
Steven BOEHM <steven.boehm.dev@gmail.com>
2022-08-10 15:05:12 +0200

    WIP clarifying / reafcto / cleanup  the import script

--- scripts/handle_release_xml.py
+++ scripts/handle_release_xml.py
@@ -1,20 +1,132 @@
+import json
 import os
+import sys
+import xml.etree.ElementTree as etree
+import urllib
+import time
 from typing import List
 
-def list_saved_release_id() -> List[int]:
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    images_dir = os.path.join(current_dir, '../images')
-    saved_releases = []
-    for path, subdirs, files in os.walk(images_dir):
-        for name in files:
-            release_id = int(name.split("_")[0])
-            saved_releases.append(release_id)
 
-    saved_releases.sort()
-    return saved_releases
+class DiscogsReleasesXMLParser:
+    """TODO."""
 
-def get_latest_saved_release_id() -> int:
-    saved_release_ids = list_saved_release_id()
-    return saved_release_ids[-1]
+    def __init__(self, discogs_client, input_file, images_dir):
+        """TODO."""
+        self.discogs_client = discogs_client
+        self.input_file = input_file
+        self.images_dir = images_dir
 
-print(get_latest_saved_release_id())
+    def list_saved_release_id(self) -> List[int]:
+        """TODO."""
+        saved_releases = []
+        for path, subdirs, files in os.walk(self.images_dir):
+            for name in [f for f in files if "primary" in f]:
+                release_id = int(name.split("_")[0])
+                saved_releases.append(release_id)
+
+        saved_releases.sort()
+        return saved_releases
+
+    def get_latest_saved_release_id(self) -> int:
+        """TODO."""
+        saved_release_ids = self.list_saved_release_id()
+        return saved_release_ids[-1]
+
+    def parse(self):
+        """TODO."""
+        start_time = time.time()
+        latest_saved_release_id = self.get_latest_saved_release_id()
+        for event, elem in etree.iterparse(self.input_file, events=("start",)):
+            if elem.tag == "release":
+                formats = elem.find('formats')
+                is_vinyl_release = formats is not None and any(f.get('name') == 'Vinyl' for f in formats)
+                master = elem.find('master_id')
+
+                if is_vinyl_release and master is not None:
+                    release_id = elem.get('id')
+                    if int(release_id) <= latest_saved_release_id:
+                        print("Already parsed release {}".format(release_id))
+                        elem.clear()
+                        continue
+                    master_id = master.text
+
+                    try:
+                        front_cover_url = get_front_cover_url(self.discogs_client, elem)
+                    except AlbumCoverURLNotFound as err:
+                        print(err, file=sys.stderr)
+                        elem.clear()
+                        continue
+
+                    filename, file_extension = os.path.splitext(front_cover_url)
+                    filename = "{}_{}_primary{}".format(release_id, master_id, file_extension)
+                    filepath = os.path.join(self.images_dir, filename)
+                    if os.path.exists(filepath):
+                        elem.clear()
+                        continue
+                    end_time = time.time()
+                    ellapsed_time = end_time - start_time
+                    sleep_time = max((1.02 - ellapsed_time), 0)
+                    time.sleep(sleep_time)
+                    try:
+                        urllib.request.urlretrieve(front_cover_url, filepath)
+                    except:
+                        print(
+                            "Downloading cover failed for release {} ({})"
+                            .format(release_id, front_cover_url)
+                        )
+                    start_time = time.time()
+                    print(filepath)
+
+            elem.clear()
+
+
+class AlbumCoverURLNotFound(Exception):
+    """Raise when we cannot find an album's cover."""
+
+    pass
+
+
+def get_front_cover_url(discogs_client, xml_release) -> str:
+    """Return the given release's front cover URL.
+
+    Args:
+        discogs_client: an authenticated instance of discogs_client
+        xml_release: a <release> ElementTree from a discogs release dump
+
+    Returns:
+        str: The URL of the album relase front cover.
+
+    Raises:
+        AlbumCoverURLNotFound: The front cover URL could not been found.
+    """
+    release_id = xml_release.get('id')
+    xml_master = xml_release.find('master_id')
+    if xml_master is None or xml_master.text is None:
+        raise AlbumCoverURLNotFound(
+            "Could not find master_id associated with release {}"
+            .format(release_id)
+        )
+    try:
+        release = discogs_client.release(release_id)
+    except json.decoder.JSONDecodeError:
+        raise AlbumCoverURLNotFound(
+            "Decoding release {} failed".format(release_id)
+        )
+    except:
+        raise AlbumCoverURLNotFound(
+            "Discogs client failed fetching release {}".format(release_id)
+        )
+    if release.images is None:
+        raise AlbumCoverURLNotFound(
+            "No images for release {}".format(release_id)
+        )
+    front_cover = next(
+        filter(lambda x: x.get('type') == 'primary', release.images),
+        None
+    )
+    if front_cover is None:
+        raise AlbumCoverURLNotFound(
+            "No front cover for release {}".format(release_id)
+        )
+
+    return front_cover.get('uri')
--- scripts/import-covers.py
+++ scripts/import-covers.py
@@ -1,69 +1,20 @@
 import os
-import glob
 import dotenv
 import discogs_client
-import xml.etree.ElementTree as etree
-import pathlib
-import urllib
-import time
-
+from handle_release_xml import DiscogsReleasesXMLParser
 
 dotenv.load_dotenv()
 
-d = discogs_client.Client(
+discogs_client = discogs_client.Client(
     'Project33T/1.0',
     user_token=os.getenv('DISCOGS_TOKEN')
 )
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-source = os.path.join(cur_dir, '../ressources/discogs_20220601_releases.xml')
+input_file = os.path.join(cur_dir, '../ressources/discogs_20220601_releases.xml')
+images_dir = os.path.join(cur_dir, '../images/')
 
-def already_saved_cover(release_id: str, master_id: str) -> bool:
-    master_dir = os.path.join(cur_dir, '../images/', master_id)
-    if not os.path.exists(master_dir):
-        return False
-    filematches = glob.glob(os.path.join(master_dir, "{}_front_cover.*".format(release_id)))
-    return len(filematches) > 0
 
-start_time = time.time()
-for event, elem in etree.iterparse(source, events=("start",)):
-    if elem.tag == "release":
-        formats = elem.find('formats')
-        is_vinyl_release = formats is not None and any(f.get('name') == 'Vinyl' for f in formats)
-        master = elem.find('master_id')
+parser = DiscogsReleasesXMLParser(discogs_client, input_file, images_dir)
 
-        if is_vinyl_release and master is not None:
-            release_id = elem.get('id')
-            master_id = master.text
-            if master_id is None:
-                print("No master for release {}".format(release_id))
-                continue
-            if already_saved_cover(release_id, master_id):
-                print("Already saved cover for album {}/{}".format(master_id, release_id))
-                continue
-            release = d.release(release_id)
-            if release.images is None:
-                print("No images for album {}/{}".format(master_id, release_id))
-                continue
-            front_cover = next(filter(lambda x: x.get('type') == 'primary', release.images), None)
-            if front_cover is None:
-                continue
-            front_cover_uri = front_cover.get('uri')
-            filename, file_extension = os.path.splitext(front_cover_uri)
-            filedir = os.path.join(cur_dir, '../images/', master_id)
-            pathlib.Path(filedir).mkdir(parents=True, exist_ok=True)
-
-            filename = "{}_front_cover{}".format(release_id, file_extension)
-            filepath = os.path.join(filedir, filename)
-            if os.path.exists(filepath):
-                continue
-            end_time = time.time()
-            ellapsed_time = end_time - start_time
-            sleep_time = max((1.02 - ellapsed_time), 0)
-            time.sleep(sleep_time)
-            urllib.request.urlretrieve(front_cover_uri, filepath)
-            start_time = time.time()
-            time_between_requests = ellapsed_time + sleep_time
-            print(filepath)
-
-        elem.clear()
+parser.parse()
